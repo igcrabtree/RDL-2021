1cb947a4d8776a6bb668e652e26590ef41275c7f
igcrabtree <60719546+igcrabtree@users.noreply.github.com>
2022-06-23 10:22:20 -0400

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,19 @@
+﻿UPDATE Teams
+SET Name = 'TECH Garage A', City = 'Boca Raton', State = 'FL', division = 1
+WHERE Id = 1;
+
+UPDATE Teams
+SET Name = 'TECH Garage B', City = 'Boca Raton', State = 'FL', division = 1
+WHERE Id = 2;
+
+UPDATE Teams
+SET Name = 'TECH Garage C', City = 'Boca Raton', State = 'FL', division = 1
+WHERE Id = 3;
+
+UPDATE Teams
+SET Name = 'TECH Garage G', City = 'Boca Raton', State = 'FL', division = 1
+WHERE Id = 7;
+
+UPDATE Teams
+SET Name = 'TECH Garage H', City = 'Boca Raton', State = 'FL', division = 1
+WHERE Id = 8;
---
+++ SQLQuery2.sql
@@ -0,0 +1,44 @@
+﻿SET IDENTITY_INSERT Teams ON;
+
+INSERT INTO Teams (Id, Name, Score, City, State, winner, division)
+VALUES (21, 'TECH Garage J', 0, 'Boca Raton', 'FL', 0, 2);
+
+INSERT INTO Teams (Id, Name, Score, City, State, winner, division)
+VALUES (22, 'TECH Garage K', 0, 'Boca Raton', 'FL', 0, 2);
+
+INSERT INTO Teams (Id, Name, Score, City, State, winner, division)
+VALUES (23, 'TECH Garage L', 0, 'Boca Raton', 'FL', 0, 2);
+
+INSERT INTO Teams (Id, Name, Score, City, State, winner, division)
+VALUES (24, 'TECH Garage M', 0, 'Boca Raton', 'FL', 0, 2);
+
+INSERT INTO Teams (Id, Name, Score, City, State, winner, division)
+VALUES (25, 'TECH Garage N', 0, 'Boca Raton', 'FL', 0, 2);
+
+INSERT INTO Teams (Id, Name, Score, City, State, winner, division)
+VALUES (26, 'TECH Garage O', 0, 'Boca Raton', 'FL', 0, 2);
+
+INSERT INTO Teams (Id, Name, Score, City, State, winner, division)
+VALUES (27, 'TECH Garage P', 0, 'Boca Raton', 'FL', 0, 2);
+
+INSERT INTO Teams (Id, Name, Score, City, State, winner, division)
+VALUES (28, 'TECH Garage Q', 0, 'Boca Raton', 'FL', 0, 2);
+
+INSERT INTO Teams (Id, Name, Score, City, State, winner, division)
+VALUES (29, 'JV Henderson', 0, 'Boca Raton', 'FL', 0, 2);
+
+INSERT INTO Teams (Id, Name, Score, City, State, winner, division)
+VALUES (30, 'B Squad Henderson', 0, 'Boca Raton', 'FL', 0, 2);
+
+INSERT INTO Teams (Id, Name, Score, City, State, winner, division)
+VALUES (200, 'LEE VARSITY', 0, 'Lee County', 'VA', 0, 2);
+
+INSERT INTO Teams (Id, Name, Score, City, State, winner, division)
+VALUES (202, 'BLUE BLAZER TECH A', 0, 'Baconton', 'GA', 0, 2);
+
+INSERT INTO Teams (Id, Name, Score, City, State, winner, division)
+VALUES (203, 'BLUE BLAZER TECH B', 0, 'Baconton', 'GA', 0, 2);
+
+INSERT INTO Teams (Id, Name, Score, City, State, winner, division)
+VALUES (204, 'BLUE BLAZER TECH C', 0, 'Baconton', 'GA', 0, 2);
+
